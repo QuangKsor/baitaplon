01430300640437b8d97a0ae5e7e1a599d98ea12f
QuangKsor <quang4351050993@st.qnu.edu.vn>
2025-06-01 23:57:23 +0700

    up lần đầu

---
+++ chucnang/timkiem/timkiem.php
@@ -0,0 +1,4 @@
+<form method="post" name="sform" action="index.php?page_layout=tatcatimkiem">
+    <input type="text" name="stext" placeholder="Tìm kiếm sản phẩm"> 
+    <input type="submit" name="sbutton" value="Tìm kiếm">
+</form>
